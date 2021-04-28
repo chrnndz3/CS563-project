a7730c07b824f7dc31088888262c6ae39e7286a0
Cindy Hernandez <cindy@Cindys-MacBook-Air.local>
2021-04-28 16:37:23 -0500

    fixed python script for top ten packages

--- TopTen_DependencyCollaborators/download_top_packages2.py
+++ TopTen_DependencyCollaborators/download_top_packages2.py
@@ -1,61 +1,26 @@
 import json
 import traceback
-
 import requests as requests
-from bs4 import BeautifulSoup
 
 # Initialized variables
 registry_url = 'https://registry.npmjs.com/'
-replicate_url = 'https://replicate.npmjs.com/'
-npmjs_url = 'https://www.npmjs.com/package/'
-top_ten_packages_list = ["lodash", "chalk", "request", "commander", "react", "express", "debug", "async", "fs-extra",
-                         "moment"]
-# top_ten_packages_list = ["request"]
-file = open("../TopTen_DependencyCollaborators/top_ten_packages_details.json", "w")
-file2 = open("../TopTen_DependencyCollaborators/top_ten_packages_average.json", "w")
-
-
-# Crawler for npmjs
-def crawler(package_name):
-    # print("Going to crawl " + package_name)
-    collaborators_list = []
-    url = npmjs_url + package_name
-
-    try:
-        npmjs_request = requests.get(url, headers={'User-Agent': 'Chrome/35.0.1916.47'}).text
-        npm_bs = BeautifulSoup(npmjs_request, 'html.parser')
-        body = npm_bs.find('body')
-        # collaborators_div = body.find_all("div", {"class": "w-100"})
-        collaborators_div = body.find("ul", {"class": "list pl0 cf"})
-
-        for li in collaborators_div:
-            a = li.find('a')
-            username = str(a['href']).replace('/~', '')
-            collaborators_list.append(username)
-    except:
-        print('[Error] while crawling npmjs for package: ' + package_name + ' and url: ' + url)
-        print("body ------ " + str(body))
-        traceback.print_exc()
-        return collaborators_list
-
-    return collaborators_list
+# top_ten_packages_list = ["lodash", "chalk", "request", "commander", "react", "express", "debug", "async", "fs-extra",
+#                          "moment"]
+top_ten_packages_list = ["lodash", "react", "chalk", "tslib", "axios", "express", "commander", "request", "moment", "react-dom"]
+file = open("new_results/top_ten_packages_details.json", "w")
+file2 = open("new_results/top_ten_packages_average.json", "w")
 
 
 def metadata_extraction(package_name):
     error = "{\"error\":\"not_found\",\"reason\": \"missing\"}"
 
-    url = replicate_url + package_name
-    replicate_request = requests.get(url)
+    url = registry_url + package_name
+    registry_url_request = requests.get(url)
     # Converts data to a json object
-    data = replicate_request.json()
+    data = registry_url_request.json()
 
     if "error" in data:
         print("There's no data in the replicate endpoint for " + package_name + ". Checking registry endpoint.")
-
-        url = registry_url + package_name
-        registry_request = requests.get(registry_url + package_name)
-        # Converts data to a json object
-        data = registry_request.json()
 
     try:
         # dist_tags contains the latest version this package is on
@@ -70,48 +35,57 @@ def metadata_extraction(package_name):
         print('[Error] at metadata_extraction_dependencies while retrieving data from ' + package_name)
         traceback.print_exc()
 
+    # ----- Maintainers -----
+    maintainers = data["maintainers"] if ("maintainers" in data) else None
+
     # ----- Dependencies -----
     dependencies = version_data["dependencies"] if ("dependencies" in version_data) else None
 
     # ----- Dev Dependencies -----
     dev_dependencies = version_data["devDependencies"] if ("devDependencies" in version_data) else None
 
-    # Crawl npmjs URL
-    collaborator_list = crawler(package_name)
-
     # ----- Create Map of all relevant information for the top 10 packages -----
     package_map = {"package_name": package_name, "version": latest_version,
-                   "collaborators": len(collaborator_list), "dependencies": dependencies,
+                   "maintainers": len(maintainers), "dependencies": dependencies,
                    "dev_dependencies": dev_dependencies}
 
     return package_map
 
 
+def maintainer_extraction(package_name):
+    url = registry_url + package_name
+    registry_url_request = requests.get(url)
+    data = registry_url_request.json()
+
+    if "error" in data:
+        print("There's no data in the replicate endpoint for " + package_name + ". Checking registry endpoint.")
+
+    # ----- Maintainers -----
+    maintainers = data["maintainers"] if ("maintainers" in data) else None
+
+    return len(maintainers)
+
+
 def create_dependency_map(transitive_dependencies):
     total_collaborators = 0
+    total_dependencies = 0
     dependencies_map = {}
 
     if (transitive_dependencies is None) or (len(transitive_dependencies) == 0):
-        # dependencies_map["average_collaborators"] = 0
         return None
     else:
-        size = len(transitive_dependencies)
-        # print("transitive_dependencies -----" + str(transitive_dependencies))
-
+        total_dependencies = len(transitive_dependencies)
         for item in transitive_dependencies.items():
             dependency_name = item[0]
 
-            # Crawl npmjs URL
-            collaborators_list = crawler(dependency_name)
+            collaborators_per_dependency = maintainer_extraction(dependency_name)
 
             # ----- Create Map of all relevant information for the top 10 packages -----
-            package_map = {"collaborators": len(collaborators_list)}
-            total_collaborators += len(collaborators_list)
-            dependencies_map[dependency_name] = package_map
+            total_collaborators += collaborators_per_dependency
 
         dependencies_map["total_collaborators"] = total_collaborators
-        dependencies_map["total_dependencies"] = size
-        dependencies_map["average_collaborators"] = (total_collaborators / size)
+        dependencies_map["total_dependencies"] = total_dependencies
+        dependencies_map["average_collaborators"] = (total_collaborators / total_dependencies)
 
     return dependencies_map
 
@@ -127,7 +101,7 @@ def main():
         dev_transitive_dependencies = package_map.get("dev_dependencies")
 
         full_package_map = {"package_name": package_map["package_name"], "version": package_map["version"],
-                            "collaborators": package_map["collaborators"], "dependencies": transitive_dependencies,
+                            "collaborators": package_map["maintainers"], "dependencies": transitive_dependencies,
                             "dev_dependencies": dev_transitive_dependencies}
 
         file.write(json.dumps(full_package_map) + "\n")
@@ -136,21 +110,11 @@ def main():
         transitive_dependencies_map = create_dependency_map(transitive_dependencies)
         dev_transitive_dependencies_map = create_dependency_map(dev_transitive_dependencies)
 
-        # sum_average = 0
-        # if ("average_collaborators" in transitive_dependencies_map) and (
-        #         "average_collaborators" in dev_transitive_dependencies_map):
-        #     sum_average = transitive_dependencies_map.get("average_collaborators") + dev_transitive_dependencies_map.get(
-        #         "average_collaborators")
-        # else:
-        #     sum_average = 0
-
         # ----- Create Map of all relevant information for the top 10 packages -----
-
         full_package_map2 = {"package_name": package_map["package_name"], "version": package_map["version"],
-                             "collaborators": package_map["collaborators"],
+                             "collaborators": package_map["maintainers"],
                              "dependencies": transitive_dependencies_map,
                              "dev_dependencies": dev_transitive_dependencies_map}
-                             # "average_transitive_dependencies_collaborators": dev_transitive_dependencies_map}
 
         file2.write(json.dumps(full_package_map2) + "\n")
 
